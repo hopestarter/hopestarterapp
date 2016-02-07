278e2b4f4ee4f5a4bb742fd99da2539c05980586
Konstantinos Koukopoulos <kostas@hopestarter.org>
2016-02-07 20:39:36 +0200

    add csrf exempt signup form

--- src/hopestarter/settings/local.py
+++ src/hopestarter/settings/local.py
@@ -9,3 +9,5 @@ MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware', )
 SITE_ID = 1
 
 GRAPPELLI_INDEX_DASHBOARD = 'hopestarter.dashboard.CustomIndexDashboard'
+
+ACCOUNT_SIGNUP_PASSWORD_VERIFICATION = False
--- src/hopestarter/urls/main.py
+++ src/hopestarter/urls/main.py
@@ -1,10 +1,14 @@
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt
+
+from allauth.account.views import signup as allauth_signup
 
 from hopestarter.urls.admin import admin_patterns
 
 
 urlpatterns = [
     url(r"^$", TemplateView.as_view(template_name="index.html"), name="home"),
+    url(r'^accounts/demo_signup/', csrf_exempt(allauth_signup), name="demo_signup"),
     url(r'^accounts/', include('allauth.urls')),
 ] + admin_patterns
