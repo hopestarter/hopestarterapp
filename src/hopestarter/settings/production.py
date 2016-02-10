87c30215785d615733dca0b4c4cface31dee4969
Konstantinos Koukopoulos <kostas@hopestarter.org>
2016-02-10 16:59:08 +0200

    don't require secrets by default

--- src/hopestarter/settings/production.py
+++ src/hopestarter/settings/production.py
@@ -2,22 +2,25 @@ import raven
 from .world import *
 from .utils import get_env_variable
 from .auth import *
-from .api_secret import *
+
+try:
+    from .api_secret import *
+    RAVEN_CONFIG = {
+        'dsn': RAVEN_DSN
+    }
+    INSTALLED_APPS += ("raven.contrib.django.raven_compat",)
+except ImportError:
+    pass
 
 DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = []
-
-INSTALLED_APPS += ("raven.contrib.django.raven_compat",)
 
 MIDDLEWARE_CLASSES = (
   'raven.contrib.django.raven_compat.middleware.Sentry404CatchMiddleware',
   'raven.contrib.django.raven_compat.middleware.SentryResponseErrorIdMiddleware',
 ) + MIDDLEWARE_CLASSES
 
-RAVEN_CONFIG = {
-    'dsn': RAVEN_DSN
-}
 
 SECRET_KEY = get_env_variable("SECRET_KEY")
 
