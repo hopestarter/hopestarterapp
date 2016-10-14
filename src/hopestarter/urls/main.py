69cf7c0d68e774251d9501124054ceef45bc6293
Rodrigo Machado <rml22388@gmail.com>
2016-10-13 22:17:58 -0300

    Profile page. Map template refactoring

--- src/hopestarter/urls/main.py
+++ src/hopestarter/urls/main.py
@@ -5,11 +5,12 @@ from django.views.decorators.csrf import csrf_exempt
 from allauth.account.views import signup as allauth_signup
 
 from hopestarter.urls.admin import admin_patterns
-from hopestarter.views.public import LocationMarkListView
+from hopestarter.views.public import LocationMarkListView, UserProfileView
 
 
 urlpatterns = [
     url(r"^$", LocationMarkListView.as_view(), name="home"),
+    url(r"^profile/(?P<pk>\d+)$", UserProfileView.as_view(), name="user-profile"),
     url(r"^index/$", TemplateView.as_view(template_name="index.html"), name="index"),
     url(r'^accounts/demo_signup/', csrf_exempt(allauth_signup), name="demo_signup"),
     url(r'^accounts/', include('allauth.urls')),
--- src/hopestarter/views/public.py
+++ src/hopestarter/views/public.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from hopespace.models import LocationMark
+from hopebase.models import UserProfile
 
 
 class LocationMarkListView(ListView):
@@ -11,3 +12,14 @@ class LocationMarkListView(ListView):
         qs = qs.select_related('user')
         qs = qs.prefetch_related('user__membership')
         return qs
+
+
+class UserProfileView(DetailView):
+
+    model = UserProfile
+
+    def get_context_data(self, **kwargs):
+        obj = super(UserProfileView, self).get_object()
+        context = super(UserProfileView, self).get_context_data(**kwargs)
+        context['user_marks'] = obj.user.marks.all()
+        return context
