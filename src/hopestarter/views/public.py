c201f5e6fe9781e8bcb8d34d1d13109c8f10f743
Rodrigo Machado <rml22388@gmail.com>
2016-10-22 01:41:48 -0300

    Use the Google Map's key configured in the settings

--- src/hopestarter/views/public.py
+++ src/hopestarter/views/public.py
@@ -1,12 +1,14 @@
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.models import User
+from django.conf import settings
+
 from hopespace.models import LocationMark
 from hopebase.models import UserProfile
 
 BOUNDARY_OFFSET = 0.1  # Boundary offset = 10%
 
-class LocationMarkListView(ListView):
 
+class LocationMarkListView(ListView):
 
     def get_queryset(self):
         qs = LocationMark.objects.exclude(user__profile=None)
@@ -14,6 +16,12 @@ class LocationMarkListView(ListView):
         qs = qs.select_related('user')
         qs = qs.prefetch_related('user__membership')
         return qs
+
+    def get_context_data(self, **kwargs):
+        context = super(LocationMarkListView, self).get_context_data(**kwargs)
+        context['google_api_key'] = settings.GOOGLE_MAPS_KEY
+
+        return context
 
 
 class UserProfileView(DetailView):
@@ -39,5 +47,7 @@ class UserProfileView(DetailView):
             'south': max_lat + (max_lat - min_lat) * BOUNDARY_OFFSET,
             'west': min_lng - (max_lng - min_lng) * BOUNDARY_OFFSET
         }
+
+        context['google_api_key'] = settings.GOOGLE_MAPS_KEY
 
         return context
