38a5c9e8ecb13c63408b2b32121baa1ed33b0a05
Konstantinos Koukopoulos <kostas@hopestarter.org>
2016-12-04 13:36:13 +0200

    hide profiles based on vetting status

--- src/hopestarter/static/javascript/ref_status.js
+++ src/hopestarter/static/javascript/ref_status.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
+  $("input[name='tabs']").change(function(){
+    var status = $(this).val();
+    $('.ref-status--profile').each(function(){
+        var profile = this;
+        if (status == "any") {
+            $(profile).show();
+        } else if ($('.vet-status', profile).hasClass('vet-status--'+status)){
+            $(profile).show();
+        } else {
+            $(profile).hide();
+        }
+    });
+  });
+
   $('.vet-status').click(function(){
-    if( $(this).hasClass('vet-status--unvetted') ) { 
+    if( $(this).hasClass('vet-status--unvetted') ) {
       $(this).removeClass('vet-status--unvetted');
       $(this).addClass('vet-status--vetted');
       $(this).html('<span class="icon-vetted"></span>Vetted');
