d72449c66d3f4933e884fa40f05a353c7e2ea19a
Konstantinos Koukopoulos <kostas@hopestarter.org>
2016-12-04 11:34:05 +0200

    add organization admin modules

--- src/hopebase/admin.py
+++ src/hopebase/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
-from hopebase.models import UserProfile
+from hopebase.models import (
+    UserProfile, Organization, OrganizationMembership
+)
 
 
 class UserProfileAdmin(admin.ModelAdmin):
-    fields = ('name', 'surname', 'bitcoin', 'created', 'modified', 'user', 'picture_tag', 'picture')
+    fields = ('name', 'surname', 'bitcoin', 'created', 'modified', 'user',
+              'picture_tag', 'picture')
     readonly_fields = ('created', 'modified', 'user', 'picture_tag')
 
 
+class OrganizationMembershipInline(admin.TabularInline):
+    model = OrganizationMembership
+    extra = 1
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+    fields = ('name', 'owner', 'created', 'modified', 'id')
+    readonly_fields = ('created', 'modified', 'id')
+    inlines = (OrganizationMembershipInline,)
+
+
+class OrganizationMembershipAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'modified')
+    search_fields = ('^person__username', '^organization__name')
+
+
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(OrganizationMembership, OrganizationMembershipAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)
