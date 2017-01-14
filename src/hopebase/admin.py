f86529055945c2f6e77fa1665a3c0003f5885022
Konstantinos Koukopoulos <kostas@hopestarter.org>
2017-01-14 12:46:02 +0200

    update hopebase admin site

--- src/hopebase/admin.py
+++ src/hopebase/admin.py
@@ -1,32 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 
+from django.core.urlresolvers import reverse
+
+from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 
 from hopebase.models import (
     UserProfile, Organization, OrganizationMembership, Vetting
 )
-
-
-class UserProfileAdmin(admin.ModelAdmin):
-    fields = ('name', 'surname', 'bitcoin', 'created', 'modified', 'user',
-              'picture_tag', 'picture', 'signup')
-    readonly_fields = ('created', 'modified', 'user', 'picture_tag')
-
-
-class OrganizationMembershipInline(admin.TabularInline):
-    model = OrganizationMembership
-    extra = 1
-
-
-class OrganizationAdmin(admin.ModelAdmin):
-    fields = ('name', 'owner', 'created', 'modified', 'id')
-    readonly_fields = ('created', 'modified', 'id')
-    inlines = (OrganizationMembershipInline,)
-
-
-class OrganizationMembershipAdmin(admin.ModelAdmin):
-    readonly_fields = ('created', 'modified')
-    search_fields = ('^person__username', '^organization__name')
 
 
 class RevokedFilter(admin.SimpleListFilter):
@@ -64,14 +47,94 @@ class RevokedFilter(admin.SimpleListFilter):
         return queryset
 
 
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    max_num = 1
+    can_delete = False
+
+
+class OrganizationMembershipInline(admin.StackedInline):
+    model = OrganizationMembership
+    extra = 0
+    can_delete = False
+
+
+class UserAdmin(AuthUserAdmin):
+    inlines = [UserProfileInline, OrganizationMembershipInline]
+    list_display = ('username', 'email', 'date_joined', 'last_login', 'profile_link')
+
+    def profile_link(self, obj):
+        return mark_safe('<a href="{}">{}</a>'.format(
+            reverse("admin:hopebase_userprofile_change", args=(obj.profile.pk,)), obj.profile))
+    profile_link.short_description = 'profile'
+
+    def get_queryset(self, request):
+        return super(UserAdmin,self).get_queryset(request).prefetch_related('profile')
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    fields = ('name', 'surname', 'bitcoin', 'created', 'modified',
+              'picture_tag', 'picture', 'signup', 'user_link')
+    readonly_fields = ('created', 'modified', 'picture_tag', 'user_link')
+    list_display = ('name', 'surname', 'bitcoin', 'created', 'modified', 'user_link')
+
+    def user_link(self, obj):
+        return mark_safe('<a href="{}">{}</a>'.format(
+            reverse("admin:auth_user_change", args=(obj.user.pk,)), obj.user))
+    user_link.short_description = 'user'
+
+
+class OrganizationMembershipInline(admin.TabularInline):
+    model = OrganizationMembership
+    extra = 1
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+    fields = ('name', 'owner', 'created', 'modified', 'id')
+    readonly_fields = ('created', 'modified', 'id')
+    inlines = (OrganizationMembershipInline,)
+
+
+class OrganizationMembershipAdmin(admin.ModelAdmin):
+    fields = ('organization', 'created', 'modified', 'revoked')
+    readonly_fields = ('user_link', 'created', 'modified', 'organization')
+    search_fields = ('^person__username', '^organization__name')
+    list_display = ('user_link', 'organization', 'created', 'modified', 'revoked')
+    list_filter = (RevokedFilter,)
+
+    def user_link(self, obj):
+        return mark_safe('<a href="{}">{}</a>'.format(
+            reverse("admin:auth_user_change", args=(obj.person.pk,)), obj.person))
+    user_link.short_description = 'user'
+
+
 class VettingAdmin(admin.ModelAdmin):
-    readonly_fields = ('created', 'modified')
+    readonly_fields = ('subject_link', 'reviewer_link', 'created', 'reviewer', 'modified', 'organization')
     ordering_fields = ('created', 'modified')
     list_filter = (RevokedFilter,)
     search_fields = ('^subject__username', '^organization__name',
                      '^reviewer__person__username')
+    list_display = ('subject_link', 'reviewer_link', 'organization', 'created', 'modified', 'revoked')
+
+    def get_queryset(self, request):
+        return super(VettingAdmin,self).get_queryset(request).select_related('subject', 'reviewer__person', 'organization')
+
+    def subject_link(self, obj):
+        return self.user_link(obj.subject)
+    subject_link.short_description = 'subject'
+
+    def reviewer_link(self, obj):
+        return self.user_link(obj.reviewer.person)
+    reviewer_link.short_description = 'reviewer'
+
+    def user_link(self, user):
+        return mark_safe('<a href="{}">{}</a>'.format(
+            reverse("admin:auth_user_change", args=(user.pk,)), user))
 
 
+user_model = get_user_model()
+admin.site.unregister(user_model)
+admin.site.register(user_model, UserAdmin)
 admin.site.register(Organization, OrganizationAdmin)
 admin.site.register(OrganizationMembership, OrganizationMembershipAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)
